33be3b1f259d43bbcdfdf666bf8e4a938508394e
MJebran <jebran.mustafa@gmail.com>
2024-03-18 11:17:54 -0600

    workflow6

--- MobileApp/TicketWebApp/DiagnosticsConfiguratoin.cs
+++ MobileApp/TicketWebApp/DiagnosticsConfiguratoin.cs
@@ -6,5 +6,5 @@ public class DiagnosticsConfiguration
     public static readonly string SourceName1 = "Mustafa_Trace_One";
     public static readonly string SourceName2 = "Mustafa_Trace_Two";
     public static readonly ActivitySource MyActivitySource1 = new(SourceName1);
-    public static readonly ActivitySource MyActivitySource2 = new (SourceName2);
+    public static readonly ActivitySource MyActivitySource2 = new(SourceName2);
 }
